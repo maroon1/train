619f42727c17d3e4fa4c18fb090e4cea4635fc5a
刘晓军 <sage2257@outlook.com>
2023-03-06 18:36:20 +0800

    feat: 完成轮播图的大部分功能
    还有悬停暂停的功能还未实现

--- scripts/official-site/main.js
+++ scripts/official-site/main.js
@@ -135,3 +135,185 @@
     }
   });
 })();
+
+(function () {
+  const bannerInfos = [
+    {
+      image: './assets/official-site/banner1.jpg',
+      title: '开启互联网+ 从我们开始',
+      description: '域名主机，网站建设，云服务器，企业邮箱一站式解决',
+    },
+    {
+      image: './assets/official-site/banner2.jpg',
+      title: '新闻中心',
+      description:
+        '几乎所有的伟大成就，都是团队集体协作追求远大目标的结果。这些团队的领导者挑选了团队的成员，并激励他们追求自己不敢想象的成就。',
+    },
+    {
+      image: './assets/official-site/banner3.jpg',
+      title: '关于我们',
+      description:
+        '企业构建互联网信息技术服务平台，领先技术变革，提升产业效率，致力于使能软件企业引领发展，服务制造企业转型升级，为政企客户提供“多快好省”的信息技术服务。',
+    },
+  ];
+
+  /** @type {HTMLDivElement[]} */
+  const bannerDoms = [];
+
+  /** @type {HTMLDivElement} */
+  const $carouselContainer = document.querySelector('.carousel__container');
+
+  /** @type {HTMLDivElement} */
+  const $carouselIndicator = document.querySelector('.carousel__indicator');
+
+  const indicatorItems = createIndicators();
+
+  let counter = 0;
+  /** @type {HTMLDivElement} */
+  let currentBanner = null;
+  /** @type {HTMLDivElement} */
+  let currentIndicator = null;
+  /** @type {Animation} */
+  let animation = null;
+
+  indicatorItems.forEach((item) => {
+    $carouselIndicator.append(item);
+  });
+
+  indicatorItems.forEach((item, i) => {
+    console.log(item);
+    item.addEventListener('click', function () {
+      console.log('click item');
+      counter = i;
+
+      bannerDoms.forEach((banner) => {
+        banner.style.zIndex = -1;
+      });
+
+      animation.cancel();
+      start(counter);
+    });
+  });
+
+  function start(currentIndex) {
+    const index = currentIndex % bannerInfos.length;
+
+    if (!bannerDoms[index]) {
+      const bannerDom = createBannerDom(index);
+      bannerDoms[index] = bannerDom;
+      $carouselContainer.append(bannerDom);
+    }
+
+    const banner = bannerDoms[index];
+    const indicator = indicatorItems[index];
+
+    if (currentBanner) {
+      currentBanner.classList.add('fadeout');
+      currentBanner.classList.remove('fadein');
+    }
+
+    if (currentIndicator) {
+      currentIndicator.classList.remove('carousel__indicator-item--active');
+    }
+
+    currentBanner = banner;
+    currentBanner.classList.add('fadein');
+    currentBanner.classList.remove('fadeout');
+    currentBanner.style.zIndex = currentIndex;
+
+    currentIndicator = indicator;
+    currentIndicator.classList.add('carousel__indicator-item--active');
+
+    /** @type {HTMLElement | undefined} */
+    const $innerBar = Array.from(
+      document.querySelectorAll('.progress__inner-bar'),
+    ).find((el) => matchParent(el.parentElement, currentIndicator));
+
+    animation = $innerBar.animate(
+      [
+        {
+          strokeDashoffset: 2 * Math.PI * 40,
+        },
+        {
+          strokeDashoffset: 0,
+        },
+      ],
+      5000,
+    );
+
+    animation.onfinish = () => {
+      counter += 1;
+      start(counter);
+    };
+  }
+
+  start(counter);
+
+  function createBannerDom(index) {
+    const banner = bannerInfos[index];
+
+    const $carouselTitle = document.createElement('div');
+    $carouselTitle.classList.add('carousel__title', 'sm:text-6xl');
+    $carouselTitle.textContent = banner.title;
+
+    const $carouselDescription = document.createElement('div');
+    $carouselDescription.classList.add(
+      'carousel__description',
+      'sm:mt-6',
+      'sm:text-3xl',
+    );
+    $carouselDescription.textContent = banner.description;
+
+    const $carouselContent = document.createElement('div');
+    $carouselContent.classList.add('carousel__content', 'sm:justify-center');
+    $carouselContent.style.backgroundImage = `url(${banner.image})`;
+    $carouselContent.append($carouselTitle);
+    $carouselContent.append($carouselDescription);
+
+    const $carouselItem = document.createElement('div');
+    $carouselItem.classList.add('carousel__item');
+    $carouselItem.append($carouselContent);
+
+    return $carouselItem;
+  }
+
+  function createIndicators() {
+    return Array.from({ length: bannerInfos.length }).map(() => {
+      const $indicatorItem = document.createElement('div');
+      $indicatorItem.classList.add('carousel__indicator-item');
+      $indicatorItem.innerHTML = `
+        <svg
+          class="progress"
+          viewBox="0 0 100 100"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <circle
+            class="progress__inner-bar"
+            cx="50"
+            cy="50"
+            r="40"
+          ></circle>
+          <circle class="progress__bar" cx="50" cy="50" r="40" />
+        </svg>
+      `;
+
+      return $indicatorItem;
+    });
+  }
+
+  /**
+   * @param {HTMLElement} node
+   * @param {HTMLElement} parent
+   */
+  function matchParent(node, parent) {
+    if (!node.parentElement) {
+      return false;
+    }
+
+    if (node.parentElement === parent) {
+      return true;
+    }
+
+    return matchParent(node.parentElement, parent);
+  }
+})();
