9a5db81646b39790e226a4b4febd194dae21857d
刘晓军 <sage2257@outlook.com>
2023-03-07 17:01:22 +0800

    feat: 添加分页查询功能

---
+++ scripts/official-site/common.js
@@ -0,0 +1,166 @@
+let newsService;
+
+const animationObserver = new IntersectionObserver(
+  (entries) => {
+    for (const entry of entries) {
+      if (entry.intersectionRatio === 0) {
+        return;
+      }
+
+      const { animation } = entry.target.dataset;
+
+      if (!animation) {
+        return;
+      }
+      entry.target.classList.remove('invisible');
+      entry.target.classList.add(animation);
+      animationObserver.unobserve(entry.target);
+    }
+  },
+  {
+    threshold: 0.25,
+  },
+);
+
+const lazyloadObserver = new IntersectionObserver(
+  (entries) => {
+    for (const entry of entries) {
+      if (entry.intersectionRatio === 0) {
+        return;
+      }
+
+      const { lazy } = entry.target.dataset;
+
+      entry.target.src = lazy;
+      lazyloadObserver.unobserve(entry.target);
+    }
+  },
+  {
+    threshold: 0,
+    rootMargin: '0px 0px 100px 0px',
+  },
+);
+
+/**
+ * @param {string} _image 新闻图片
+ * @param {string} title 新闻标题
+ * @param {string} summary 新闻概览
+ */
+function createNewsItem(image, title, summary) {
+  const $img = document.createElement('img');
+  $img.setAttribute('data-lazy', image);
+  $img.setAttribute('alt', '新闻插图');
+
+  const $imgWrapper = document.createElement('div');
+  $imgWrapper.classList.add('news__image', 'hover-scale');
+  $imgWrapper.append($img);
+
+  const $title = document.createElement('h3');
+  $title.classList.add('news__title', 'sm:text-lg');
+  $title.setAttribute('data-animation', 'fadein');
+  $title.textContent = title;
+
+  const $summary = document.createElement('p');
+  $summary.classList.add('news__summary');
+  $summary.setAttribute('data-animation', 'fadein');
+  $summary.textContent = summary;
+
+  const $newsItem = document.createElement('li');
+  $newsItem.classList.add('news__item', 'sm:basis-1/4', 'sm:px-4');
+
+  $newsItem.append($imgWrapper, $title, $summary);
+
+  return $newsItem;
+}
+
+/**
+ * @param {HTMLElement[]} items
+ */
+function observeAnimationItems(items) {
+  if (items.length === 0) {
+    return;
+  }
+
+  for (const item of items) {
+    item.classList.add('invisible');
+    animationObserver.observe(item);
+  }
+}
+
+/**
+ * @param {HTMLElement[]} items
+ */
+function observeLazyloadItems(items) {
+  if (items.length === 0) {
+    return;
+  }
+
+  for (const item of items) {
+    // WHY: 第一屏中的图片无法显示
+    const bouding = item.getBoundingClientRect();
+
+    if (bouding.top < window.innerHeight) {
+      item.src = item.dataset.lazy;
+      continue;
+    }
+
+    lazyloadObserver.observe(item);
+  }
+}
+
+(function () {
+  const baseUrl = 'https://64006b1463e89b0913ae6339.mockapi.io/api/';
+
+  class NewsService {
+    newsUrl = new URL('news', baseUrl);
+
+    getNews(page, limit) {
+      const search = new URLSearchParams([
+        ['page', String(page)],
+        ['limit', String(limit)],
+      ]);
+      const url = new URL(this.newsUrl);
+      url.search = search.toString();
+
+      return fetch(url).then((res) => res.json());
+    }
+
+    getRecentNews(num) {
+      return this.getNews(1, num);
+    }
+  }
+
+  newsService = new NewsService();
+})();
+
+(function () {
+  const domObserver = new MutationObserver((mutations) => {
+    mutations.forEach((mutation) => {
+      if (
+        !mutation.target.classList ||
+        !mutation.target.classList.contains('news__list')
+      ) {
+        return;
+      }
+
+      if (mutation.addedNodes.length === 0) {
+        return;
+      }
+
+      mutation.addedNodes.forEach((node) => {
+        if (node instanceof HTMLElement) {
+          const animationItems = node.querySelectorAll('[data-animation]');
+          observeAnimationItems(animationItems);
+
+          const lazyloadItems = node.querySelectorAll('[data-lazy]');
+          observeLazyloadItems(lazyloadItems);
+        }
+      });
+    });
+  });
+
+  domObserver.observe(document.body, {
+    childList: true,
+    subtree: true,
+  });
+})();
---
+++ scripts/official-site/news.js
@@ -0,0 +1,185 @@
+(function () {
+  const url = new URL(window.location.href);
+  const TOTAL = 100;
+  let page = url.searchParams.get('page') || '1';
+  let limit = url.searchParams.get('limit') || '8';
+
+  /** @type {HTMLSelectElement} */
+  const $paginationSizer = document.querySelector('.pagination__sizer');
+  $paginationSizer.querySelectorAll('option').forEach((item) => {
+    item.selected = false;
+
+    if (item.value === limit) {
+      item.selected = true;
+    }
+  });
+
+  $paginationSizer.addEventListener('change', (e) => {
+    const url = new URL(window.location.href);
+    url.searchParams.set('limit', e.target.value);
+    window.location.href = url.toString();
+  });
+
+  /** @type {HTMLInputElement} */
+  const $paginationInput = document.querySelector('.pagination__jumper-input');
+
+  $paginationInput.addEventListener('keydown', (e) => {
+    if (e.key !== 'Enter') {
+      return;
+    }
+
+    if (e.target.value === '') {
+      return;
+    }
+
+    const url = new URL(window.location.href);
+    url.searchParams.set('page', e.target.value);
+    window.location.href = url.toString();
+  });
+
+  generatePagination(TOTAL, +page, +limit);
+
+  const $newsList = document.querySelector('.news__list');
+
+  newsService.getNews(page, limit).then((newsList) => {
+    $newsList.innerHTML = '';
+    newsList.forEach((news) => {
+      const $newItem = createNewsItem(
+        'https://loremflickr.com/270/420?lock=1',
+        news.title,
+        news.summary,
+      );
+
+      $newsList.append($newItem);
+    });
+  });
+
+  function generatePagination(total, page, limit) {
+    /** @type {HTMLUListElement} */
+    const $paginationList = document.querySelector('.pagination__item-list');
+    $paginationList.innerHTML = '';
+
+    /**
+     * 开始页码
+     */
+    const startPage = 1;
+    /**
+     * 结束页码
+     */
+    const endPage = Math.ceil(total / limit);
+
+    const $prev = createPaginationItem('<');
+    $prev.classList.add('pagination__prev');
+
+    if (page === 1) {
+      $prev.classList.add('pagination__item--disabled');
+    } else {
+      setLink($prev, page - 1);
+    }
+
+    $paginationList.append($prev);
+
+    const $startItem = createPaginationItem('1');
+    setLink($startItem, 1);
+    setActive($startItem, 1, page);
+    $paginationList.append($startItem);
+
+    const TOTAL_MIDDLE_NUMBERS = 5;
+
+    const $prevJumper = createPaginationItem('...');
+    setLink(
+      $prevJumper,
+      clamp(startPage, page - TOTAL_MIDDLE_NUMBERS, endPage),
+    );
+    const $nextJumper = createPaginationItem('...');
+    setLink(
+      $nextJumper,
+      clamp(startPage, page + TOTAL_MIDDLE_NUMBERS, endPage),
+    );
+
+    const middlePageStart = clamp(
+      startPage + 1,
+      page - Math.floor(TOTAL_MIDDLE_NUMBERS / 2),
+      endPage - 1,
+    );
+
+    for (let i = 0; i < TOTAL_MIDDLE_NUMBERS; i++) {
+      const current = clamp(startPage, middlePageStart + i, endPage);
+
+      if ([startPage, endPage].includes(current)) {
+        continue;
+      }
+
+      const $item = createPaginationItem(current);
+      setLink($item, current);
+
+      if (i === 0 && current !== startPage + 1) {
+        $paginationList.append($prevJumper);
+      }
+
+      $paginationList.append($item);
+      setActive($item, current, page);
+
+      if (i === TOTAL_MIDDLE_NUMBERS - 1 && current !== endPage - 1) {
+        $paginationList.append($nextJumper);
+      }
+    }
+
+    if (startPage !== endPage) {
+      const $endItem = createPaginationItem(String(endPage));
+      setLink($endItem, endPage);
+      setActive($endItem, endPage, page);
+      $paginationList.append($endItem);
+    }
+
+    const $next = createPaginationItem('>');
+    $next.classList.add('pagination__next');
+
+    if (page === endPage) {
+      $next.classList.add('pagination__item--disabled');
+    } else {
+      setLink($next, page + 1);
+    }
+
+    $paginationList.append($next);
+  }
+
+  /**
+   * @param {HTMLLIElement} el
+   * @param {number} page
+   */
+  function setLink(el, page, limit) {
+    const url = new URL(window.location.href);
+    url.searchParams.set('page', page);
+
+    const $a = document.createElement('a');
+    $a.href = url.toString();
+
+    el.append($a);
+  }
+
+  function createPaginationItem(content) {
+    const $item = document.createElement('li');
+    $item.textContent = content;
+    $item.classList.add('pagination__item');
+
+    return $item;
+  }
+
+  function clamp(min, value, max) {
+    return Math.min(Math.max(min, value), max);
+  }
+
+  /**
+   * @param {HTMLElement} el
+   * @param {number} current
+   * @param {number} page
+   */
+  function setActive(el, current, page) {
+    if (current !== page) {
+      return;
+    }
+
+    el.classList.add('pagination__item--active');
+  }
+})();
