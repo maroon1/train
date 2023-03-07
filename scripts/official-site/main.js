2dc4ebadfd2283e327a60dae189f2529573460e8
刘晓军 <sage2257@outlook.com>
2023-03-07 13:54:41 +0800

    feat: 添加动态内容的懒加载

--- scripts/official-site/main.js
+++ scripts/official-site/main.js
@@ -1,3 +1,102 @@
+let newsService;
+
+const animationObserver = new IntersectionObserver(
+  (entries) => {
+    for (const entry of entries) {
+      if (entry.intersectionRatio === 0) {
+        return;
+      }
+
+      const { animation } = entry.target.dataset;
+
+      if (!animation) {
+        return;
+      }
+      entry.target.classList.add(animation);
+      animationObserver.unobserve(entry.target);
+    }
+  },
+  {
+    threshold: 0,
+  },
+);
+
+const lazyloadObserver = new IntersectionObserver(
+  (entries) => {
+    for (const entry of entries) {
+      if (entry.intersectionRatio === 0) {
+        return;
+      }
+
+      const { lazy } = entry.target.dataset;
+
+      entry.target.src = lazy;
+      lazyloadObserver.unobserve(entry.target);
+    }
+  },
+  {
+    threshold: 0,
+    rootMargin: '0px 0px 100px 0px',
+  },
+);
+
+(function () {
+  const domObserver = new MutationObserver((mutations) => {
+    mutations.forEach((mutation) => {
+      if (
+        !mutation.target.classList ||
+        !mutation.target.classList.contains('news__list')
+      ) {
+        return;
+      }
+
+      if (mutation.addedNodes.length === 0) {
+        return;
+      }
+
+      mutation.addedNodes.forEach((node) => {
+        if (node instanceof HTMLElement) {
+          const animationItems = node.querySelectorAll('[data-animation]');
+          observeAnimationItems(animationItems);
+
+          const lazyloadItems = node.querySelectorAll('[data-lazy]');
+          observeLazyloadItems(lazyloadItems);
+        }
+      });
+    });
+  });
+
+  domObserver.observe(document.body, {
+    childList: true,
+    subtree: true,
+  });
+})();
+
+(function () {
+  const baseUrl = 'https://64006b1463e89b0913ae6339.mockapi.io/api/';
+
+  class NewsService {
+    static newsUrl = new URL('news', baseUrl);
+
+    getNews(page, limit) {
+      const search = new URLSearchParams([
+        ['page', String(page)],
+        ['limit', String(limit)],
+      ]);
+      const url = new URL(NewsService.newsUrl);
+      url.search = search.toString();
+
+      return fetch(url).then((res) => res.json());
+    }
+
+    getRecentNews(num) {
+      return this.getNews(1, num);
+    }
+  }
+
+  newsService = new NewsService();
+})();
+
 (function () {
   document.addEventListener('DOMContentLoaded', function () {
     const $menu = document.querySelector('.header__menu');
@@ -63,34 +162,7 @@
   document.addEventListener('DOMContentLoaded', function () {
     const animationItems = document.querySelectorAll('[data-animation]');
 
-    if (animationItems.length === 0) {
-      return;
-    }
-
-    const observer = new IntersectionObserver(
-      (entries) => {
-        for (const entry of entries) {
-          if (entry.intersectionRatio === 0) {
-            return;
-          }
-
-          const { animation } = entry.target.dataset;
-
-          if (!animation) {
-            return;
-          }
-          entry.target.classList.add(animation);
-          observer.unobserve(entry.target);
-        }
-      },
-      {
-        threshold: 0,
-      },
-    );
-
-    for (const item of animationItems) {
-      observer.observe(item);
-    }
+    observeAnimationItems(animationItems);
   });
 })();
 
@@ -99,40 +171,7 @@
     /** @type {HTMLImageElement[]} */
     const lazyItems = document.querySelectorAll('img[data-lazy]');
 
-    if (lazyItems.length === 0) {
-      return;
-    }
-
-    const observer = new IntersectionObserver(
-      (entries) => {
-        for (const entry of entries) {
-          if (entry.intersectionRatio === 0) {
-            return;
-          }
-
-          const { lazy } = entry.target.dataset;
-
-          entry.target.src = lazy;
-          observer.unobserve(entry.target);
-        }
-      },
-      {
-        threshold: 0,
-        rootMargin: '0px 0px 100px 0px',
-      },
-    );
-
-    for (const item of lazyItems) {
-      // WHY: 第一屏中的图片无法显示
-      const bouding = item.getBoundingClientRect();
-
-      if (bouding.top < window.innerHeight) {
-        item.src = item.dataset.lazy;
-        continue;
-      }
-
-      observer.observe(item);
-    }
+    observeLazyloadItems(lazyItems);
   });
 })();
 
@@ -326,3 +365,85 @@
     return matchParent(node.parentElement, parent);
   }
 })();
+
+(function () {
+  const $newsList = document.querySelector('.news__list');
+
+  newsService.getRecentNews(4).then((newsList) => {
+    newsList.forEach((news) => {
+      const $newItem = createNewsItem(
+        'https://loremflickr.com/270/420?lock=1',
+        news.title,
+        news.summary,
+      );
+
+      $newsList.append($newItem);
+    });
+  });
+
+  /**
+   * @param {string} _image 新闻图片
+   * @param {string} title 新闻标题
+   * @param {string} summary 新闻概览
+   */
+  function createNewsItem(image, title, summary) {
+    const $img = document.createElement('img');
+    $img.setAttribute('data-lazy', image);
+    $img.setAttribute('alt', '新闻插图');
+
+    const $imgWrapper = document.createElement('div');
+    $imgWrapper.classList.add('news__image', 'hover-scale');
+    $imgWrapper.append($img);
+
+    const $title = document.createElement('h3');
+    $title.classList.add('news__title', 'sm:text-lg');
+    $title.setAttribute('data-animation', 'fadein');
+    $title.textContent = title;
+
+    const $summary = document.createElement('p');
+    $summary.classList.add('news__summary');
+    $summary.setAttribute('data-animation', 'fadein');
+    $summary.textContent = summary;
+
+    const $newsItem = document.createElement('li');
+    $newsItem.classList.add('news__item', 'sm:basis-1/4', 'sm:px-4');
+
+    $newsItem.append($imgWrapper, $title, $summary);
+
+    return $newsItem;
+  }
+})();
+
+/**
+ * @param {HTMLElement[]} items
+ */
+function observeAnimationItems(items) {
+  if (items.length === 0) {
+    return;
+  }
+
+  for (const item of items) {
+    animationObserver.observe(item);
+  }
+}
+
+/**
+ * @param {HTMLElement[]} items
+ */
+function observeLazyloadItems(items) {
+  if (items.length === 0) {
+    return;
+  }
+
+  for (const item of items) {
+    // WHY: 第一屏中的图片无法显示
+    const bouding = item.getBoundingClientRect();
+
+    if (bouding.top < window.innerHeight) {
+      item.src = item.dataset.lazy;
+      continue;
+    }
+
+    lazyloadObserver.observe(item);
+  }
+}
